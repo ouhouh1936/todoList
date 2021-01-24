8c77a92fd8121a59f0c96bca55791f3fa87d988a
ouhouh1936 <ouhouh1936@gamil.com>
2021-01-24 23:02:16 +0900

    else if 2

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const age = prompt("How oldare you");
+const title = document.querySelector("# title");
 
-console.log(age);
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#7f8c8d";
+function handClick() {
+  const currentColor = title.style.color;
+  if (currentColor === BASE_COLOR) {
+    title.style.color = OTHER_COLOR;
+  } else {
+    title.style.color = BASE_COLOR;
+  }
+}
+function init() {
+  title.style.color = BASE_COLOR;
+  title.addEventListener("click", handClick);
+}
+init();
