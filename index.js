adb3b040e0341aa2e57ede7818edecdc9f4b381e
ouhouh1936 <ouhouh1936@gamil.com>
2021-01-24 19:56:23 +0900

    event

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const title = document.querySelector("# title");
-title.innerHtml = `Hi `;
-title.style.color = "red";
-document.title = "I own you now";
+
+function handClick() {
+  title.style.color = "blue";
+}
+
+title.addEventListener("click", handClick);
